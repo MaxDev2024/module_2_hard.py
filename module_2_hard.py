83cbfdd2babf44087329e99df8207ae02ef53e2d
MaxDev2024 <maksimkalacky2009@gmail.com>
2024-11-06 15:00:29 +0900

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,22 @@
+def get_password(number):
+    password = ''
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+while True:
+    try:
+        n = int(input('Введите целое число от 3 до 20: '))
+        if n.is_integer() and 2 < n < 21:
+            break
+        else:
+            print('Ваше число слишком маленькое или очень большое. Нужно другое.')
+    except ValueError:
+        print('Это либо дробное число, либо строка. Совсем не то.')
+
+result = get_password(n)
+print('Пароль:', result)
